6cd74778c9620174652d7228bd0d1d4383647724
Rosa Chavez <kimrosita56@gmail.com>
2022-08-28 19:25:38 -0400

    orueba

---
+++ public/main.js
@@ -0,0 +1,14 @@
+
+const form=document.getElementById("login");
+const message=document.getElementById("message");
+form.addEventListener("submit", function(e) {   
+    e.preventDefault();
+    //console.log(e.target);
+    const formData = new FormData(form);
+    for (const [key, value] of formData) {
+        localStorage.setItem(key, value);
+        console.log(  `${key}: ${value}\n`);
+        
+      }
+      message.innerText= 'usuario logueado'
+})
--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-console.log('hola')
-// dsfwecsscdsd
+import { chromium } from "@playwright/test";
+const url= "http://127.0.0.1:5500/public/index.html";
+(async () => {
+  const browser = await chromium.launch();
+
+  const context = await browser.newContext();
+
+  const page = await context.newPage();
+  await page.goto(url)
+  await page.fill('input[name="username"]', 'admin');
+  await page.fill('input[name="password"]', 'admin');
+ // await page.click('input[name="submit"]');
+  await page.screenshot({ path: "prueba.png" });
+  await page.locator('input[name="submit"]').click();
+  await page.screenshot({ path: "prueba1.png" });
+  await page.context().storageState({path: "credentials.json"});
+  const messages = [];
+  page.on('console', msg => messages.push(`[${msg.type()}] ${msg.text()}`));
+
+  await browser.close();
+})();
 
