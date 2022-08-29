08d6cc5769201148625292dc0a1b24694b6a0f7a
Rosa Chavez <kimrosita56@gmail.com>
2022-08-29 17:28:24 -0400

    avance de login

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-console.log('hola')
-// dsfwecsscdsd
+const {chromium} = require('@playwright/test');
+const url =('http://192.168.1.17/doc/page/login.asp?_1594146088492&page=application	');
+(async () =>{
+    const browser= await chromium.launch();
+    const context = await browser.newContext();
 
+    const page = await context.NewPage();
+    await page.goto(url);
+    await page.fill('input[id="username"]','user');
+    await page.fill('input[id="password"]','User123!');
+    await page.locator('input[type="button"]');
+
+
+    await browser.close();
+})
