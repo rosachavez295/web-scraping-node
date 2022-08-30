e986cbeae47c6f6c7cb3bb826d28eff0c1882511
Rosa Chavez <kimrosita56@gmail.com>
2022-08-30 17:52:16 -0400

    scd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const {chromium} = require('@playwright/test');
 const url =('http://192.168.1.17/doc/page/login.asp?_1594146088492&page=application	');
+let date = new Date();
+let separator = "/"
+let output =  date.getFullYear()+ separator + String(date.getMonth() + 1).padStart(2, '0') 
+
+
++ separator +String(date.getDate()-1).padStart(2, '0') ;
 (async () =>{
     const browser= await chromium.launch();
     const context = await browser.newContext();
@@ -9,6 +15,8 @@ const url =('http://192.168.1.17/doc/page/login.asp?_1594146088492&page=applicat
     await page.fill('input[id="username"]','user');
     await page.fill('input[id="password"]','User123!');
     await page.locator('input[type="button"]');
+    await page.click('button');
+
 
 
     await browser.close();
