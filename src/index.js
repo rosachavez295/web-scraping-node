96268aa9aa541e12508552927a672711267a1891
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2022-08-31 21:50:57 -0400

    agrego funcion para obtener semanas

--- src/index.js
+++ src/index.js
@@ -20,8 +20,12 @@ import { baseUrl, credentials } from "./utils.js";
   //await page.locator('td[class="Wday"]').click();
   await page.click('input[class="Wdate ng-isolate-scope"]');
   await delayTime(4);
+
+  // TODO: ADD Function getWeeks
+  await getRow(page);
+
   //await page.locator(`text=${date}`).click();
-  await page.locator(".wday").locator("td");
+  // await page.locator(".wday").locator("td");
 
   await page.screenshot({ path: "prueba3.png" });
   await delayTime(4);
@@ -35,4 +39,21 @@ function delayTime(second = 1) {
       res(true);
     }, second * 1000);
   });
+}
+
+async function getRow(page) {
+  const data = [];
+  const allWeeks = await page.locator("tbody", (weeks) => {
+    return weeks.map((week) => {
+      const daysNum = [...Array(7).keys()].map((e) => e + 1);
+
+      daysNum.forEach((num) => {
+        data.push(week.querySelector(`td:nth-child(${num})`));
+      });
+    });
+  });
+  console.log(`${allWeeks.length} weeks found`);
+  console.dir(allWeeks);
+  console.dir(data);
+  return [allWeeks];
 }
