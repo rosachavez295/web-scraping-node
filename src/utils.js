4982853490e529d7e0dd4c9a63cf24384319d499
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2022-09-01 20:23:27 -0400

    agrego template para url de sucursales

--- src/utils.js
+++ src/utils.js
@@ -6,5 +6,13 @@ export const credentials = {
 export const URLs = {
   login:
     "http://192.168.1.17/doc/page/login.asp?_1594146088492&page=application",
+
+  sucursales: {
+    1: "",
+    2: "",
+    3: "",
+    4: "",
+    5: "",
+  },
 };
 export const baseUrl = URLs.login;
