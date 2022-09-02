f5977445b38329b7da2cb1574133f487e5c6f8ef
rosachavez295 <kimrosita56@gmail.com>
2022-09-01 20:26:11 -0400

    agrago url en objet

--- src/utils.js
+++ src/utils.js
@@ -8,11 +8,11 @@ export const URLs = {
     "http://192.168.1.17/doc/page/login.asp?_1594146088492&page=application",
 
   sucursales: {
-    1: "",
-    2: "",
-    3: "",
-    4: "",
-    5: "",
+    1: "http://192.168.1.17/doc/page/login.asp?_1594146088492&page=application",
+    2: "http://192.168.0.17/doc/page/login.asp?_1594147940846&page=application",
+    3: "http://192.168.2.17/doc/page/login.asp?_1594233539853&page=application",
+    4: "http://192.168.4.223/doc/page/login.asp?_1594234130464&page=application",
+    5: "http://192.168.5.17/doc/page/login.asp?_1594242388385&page=application",
   },
 };
 export const baseUrl = URLs.login;
