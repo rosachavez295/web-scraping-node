cbc37ef67d6bba479f73d7b53511eb201abf81bd
rosachavez295 <kimrosita56@gmail.com>
2022-09-09 21:05:34 -0400

    agregar extension.js

--- src/convertDataToText.js
+++ src/convertDataToText.js
@@ -1,9 +1,8 @@
 import * as fs from "node:fs/promises";
-import getCurrentDate from "./getCurrentDate";
+import getCurrentDate from "./getCurrentDate.js";
 
 export default async function convertDataToText(data, sucId) {
-  
-  const currentDate =getCurrentDate();
+  const currentDate = getCurrentDate();
   const fileName = currentDate + "_" + "suc" + "-" + sucId + ".txt";
   const pathFileName = `./sheets/${fileName}`;
   await fs.writeFile(pathFileName, data);
